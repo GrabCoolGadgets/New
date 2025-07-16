23e430128a0eacef8049cca7fc46b093cd0aa368
GrabCoolGadgets <135804074+GrabCoolGadgets@users.noreply.github.com>
2025-07-16 12:05:00 +0530

    Update remove_webhook.py

--- remove_webhook.py
+++ remove_webhook.py
@@ -1,6 +1,6 @@
 import telebot
 
-BOT_TOKEN = "8149705514:AAHTICcuHg7wBvCKbLWEImWx80"  # Apna current bot token
+BOT_TOKEN = "8149705514:AAHTICcuHg7n6Qd5c-b7wBvCKbLWEImWx80"  # Apna current bot token
 
 bot = telebot.TeleBot(BOT_TOKEN)
 bot.remove_webhook()

e43f88568ac74bfaf41c6d89d3b1ca4d99bf38bc
GrabCoolGadgets <135804074+GrabCoolGadgets@users.noreply.github.com>
2025-07-16 10:27:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-
 BOT_TOKEN = "8149705514:AAHTICcuHg7n6Qd5c-b7wBvCKbLWEImWx80"
-MONGO_URI = "YOUR_MONGO_URI_HERE"
+MONGO_URI = "mongodb+srv://Reenmayee1:Reenmayee1@cluster0.ewc3o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 UPDATE_CHANNEL = "@Click2Earning_Pro"
 ADMIN_ID = 1413767412
